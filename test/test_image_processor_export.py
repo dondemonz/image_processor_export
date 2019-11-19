42e4bca25775ed3b18a8013817d869729b552777
root <kykareky05@mail.ru>
2019-11-19 08:05:23 +0300

    24

--- test/test_image_processor_export.py
+++ test/test_image_processor_export.py
@@ -1,24 +1,25 @@
 from model.input_data import *
 import time
-#import datetime as dt
-from fixture.work_with_db import DbHelper
-#from fixture.react import ReactHelper
+
 
 def test1_image_export_process_test(fix_dll, fix_react):
     tick = "process_test"
     tm = fix_react.create_record_and_timestamp(fix_dll)
-    fix_dll.send_react(("IMAGE_EXPORT|" + objId + "|EXPORT|request_id<" + tick + ">,import<cam$" + camId + ";time$" + tm + ">,export_engine<file>,export<filename$test1_export_" + tick + "_on_cam" + camId + ";dir$" + dir + ">,export_image<format$png;quality$85>,process<color:200,50,150;penwidth:10;rect:10,20,25,15;crop:0,0,95,95;font:20;polygon:15,15,20,20,25,20,20,90,70,25,40,40,55,55;polyline:90,85,80,81,75,90,50,50;text:15,75,blabla test>,caption<15:ahaha test blabla \n phah testtest bla 123123111!!!>").encode("utf-8"))
-    time.sleep(7)
+    send_user_react(caption_data="15:ahaha test blabla \n phah testtest bla 123123111!!!", export_image_data="format$png;quality$85", process_data="color:200,50,150;penwidth:10;rect:10,20,25,15;crop:0,0,95,95;font:20;polygon:15,15,20,20,25,20,20,90,70,25,40,40,55,55;polyline:90,85,80,81,75,90,50,50;text:15,75,blabla test", tick="process_test", tm=tm, fix_dll=fix_dll)
     fix_dll.search_in_callback(par="request_id")
     time.sleep(7)
     assert fix_dll.p == tick
 
 
+def send_user_react(caption_data=None, export_image_data=None, fix_dll=None, process_data=None, tick=None, tm=None):
+    fix_dll.send_react(("IMAGE_EXPORT|" + objId + "|EXPORT|request_id<" + tick + ">,import<cam$" + camId + ";time$" + tm + ">,export_engine<file>,export<filename$test1_export_" + tick + "_on_cam" + camId + ";dir$" + dir + ">,export_image<" + export_image_data + ">,process<" + process_data + ">,caption<" + caption_data + ">").encode("utf-8"))
+    time.sleep(7)
 
 
 def test2_image_export_jpg_quality_low_test(fix_dll, fix_react):
     tick = "low_jpg_test"
     tm = fix_react.create_record_and_timestamp(fix_dll)
+    send_user_react(caption_data="25:low jpg", export_image_data="format$jpg;quality$1", fix_dll=fix_dll, tick="low_jpg_test", tm=tm)
     fix_dll.send_react(("IMAGE_EXPORT|" + objId + "|EXPORT|request_id<" + tick + ">,import<cam$" + camId + ";time$" + tm + ">,export_engine<file>,export<filename$test2_export_" + tick + "_on_cam" + camId + ";dir$" + dir + ">,export_image<format$jpg;quality$1>,caption<25:low jpg>").encode("utf-8"))
     time.sleep(5)
     fix_dll.search_in_callback(par="request_id")
@@ -65,10 +66,10 @@ def test5_image_export_arhive_and_live_image_task_in_a_row(fix_dll, fix_react):
 
 def test6_ImageExport_DBImage(fix_dll, fix_react, fix_db):
     tick = "DBImage"
+    db_str = IdDB + ":INSERT INTO public.image(image, tid) VALUES(?, '" + tid + "')"
     fix_db.create_db_and_tables()
     tm = fix_react.create_record_and_timestamp(fix_dll)
-    DBstr = IdDB+":INSERT INTO public.image(image, tid) VALUES(?, '"+tid+"')"
-    fix_dll.send_react(("IMAGE_EXPORT|" + objId + "|EXPORT|request_id<" + tick + ">,import<cam$" + camId + ";time$" + tm + ">,export_engine<sql>,export<" + DBstr + ">,export_image<format$png;quality$85>,process<color:200,50,150;penwidth:10;rect:10,20,25,15;font:20;polygon:15,15,20,20>").encode("utf-8"))
+    fix_dll.send_react(("IMAGE_EXPORT|" + objId + "|EXPORT|request_id<" + tick + ">,import<cam$" + camId + ";time$" + tm + ">,export_engine<sql>,export<" + db_str + ">,export_image<format$png;quality$85>,process<color:200,50,150;penwidth:10;rect:10,20,25,15;font:20;polygon:15,15,20,20>").encode("utf-8"))
     time.sleep(3)
     fix_dll.search_in_callback(par="request_id")
     assert fix_dll.p == tick
